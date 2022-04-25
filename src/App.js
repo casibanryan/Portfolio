a60b6b9cb9084b78252aeb1b71998a666eee17c0
casibanryan <restyochea@gmail.com>
2022-04-25 13:54:02 +0800

    adding bootstrap js cdn

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ import React, { useEffect } from 'react';
 
 function App() {
 
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src = "./js/scripts.js";
-  //   script.async = true;
-  //   document.body.appendChild(script);
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "./js/scripts.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
 
   return (
