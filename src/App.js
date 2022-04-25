281e100d99b335776bbf8cba386b1b39ff0cf5a2
casibanryan <restyochea@gmail.com>
2022-04-25 08:44:18 +0800

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from './include/Navbar';
-
+import About from './include/About';
 
 
 function App() {
   return (
-    <div id="page-top">
+    <div id="page-top" className="container-fluid p-0">
       <Navbar />
+      <About />
     </div>
   );
 }
---
+++ src/include/About.js
@@ -0,0 +1,45 @@
+
+
+const About = () => (
+    <>
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Restituto
+                    <span class="text-primary">Ochea</span>
+                </h1>
+                <div class="subheading mb-5">
+                    6015 Basak street Lapu-lapu City· Cebu Philippines · (032) 887-5584 ·
+                    <a href="mailto:restyochea.com">restyochea@gmail.com</a>
+                </div>
+                <p class="lead mb-5">
+                    I've been building websites and learning code for almost 5 years and am experienced in WordPress,
+                    HTML5, CSS3, PHP, Python3,
+                    JavaScript, JQuery, Bootstrap 5, React, and laravel. I work on a variety of
+                    projects of all sizes, and
+                    One of my specialties is taking an idea from scratch and creating a full-fledged
+                    platform. I go beyond to produce sites
+                    with a unique, outstanding, contemporary look-and-feel. With extensive knowledge of web mechanics,
+                    I'm able to optimize
+                    complex integrations to require little-to-no maintenance while running on their own for years.
+
+                    I am a programmer at heart and create features that are best suited for the job at hand.
+
+                </p>
+                <div class="social-icons">
+                    <a class="social-icon" href="https://www.linkedin.com/in/restituto-ochea-a873b3236/"
+                        target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="social-icon" href="https://github.com/casibanryan" target="_blank"><i
+                        class="fab fa-github"></i></a>
+                    <a class="social-icon" href="https://twitter.com/ochea_restituto" target="_blank"><i
+                        class="fab fa-twitter"></i></a>
+                    <a class="social-icon" href="https://www.facebook.com/TitsuKe" target="_blank"><i
+                        class="fab fa-facebook-f"></i></a>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+    </>
+)
+
+export default About;
