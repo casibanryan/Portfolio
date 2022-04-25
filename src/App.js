69a7459cc3eba0c1238c34348a6694ba2b72b876
casibanryan <restyochea@gmail.com>
2022-04-25 09:32:41 +0800

    updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from './include/Navbar';
 import About from './include/About';
 import Experience from './include/Experience';
+import { Helmet } from 'react-helmet';
+
+
 
 function App() {
   return (
@@ -8,6 +11,13 @@ function App() {
       <Navbar />
       <About />
       <Experience />
+      <Helmet>
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
+          type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <script src="../js/scripts.js" type="text/javascript" />
+      </Helmet>
     </div>
   );
 }
