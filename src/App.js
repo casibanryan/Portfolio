8f162c4aa14d2c33ad015eb893ecc66c7cee7c8c
casibanryan <restyochea@gmail.com>
2022-04-25 08:46:45 +0800

    updating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Navbar from './include/Navbar';
 import About from './include/About';
-
+import Experience from './include/Experience';
 
 function App() {
   return (
     <div id="page-top" className="container-fluid p-0">
       <Navbar />
       <About />
+      <Experience />
     </div>
   );
 }
---
+++ src/include/Experience.js
@@ -0,0 +1,36 @@
+
+const Experience = () => (
+    <>
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Experience</h2>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Freelancer</h3>
+                        <div class="subheading mb-3">Facebook</div>
+                        <p>Building website for low price but high quality.</p>
+                        <p>Helping other's with their error (QA)</p>
+
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">January 2021 - Present</span></div>
+                </div>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Associate Web Developer</h3>
+                        <div class="subheading mb-3">NeoDocto Inc.</div>
+                        <p>Created a program that has been used by the company for 6 months and building websites,
+                            After 1 month I was promoted as a group leader of web scrapping team python.</p>
+                        <p>Guiding other interns on how to web scrap data using python and how to automate websites.</p>
+                        <p>Customizing wordpress templates and updating website content.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">September 2021 - March 2022</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+    </>
+)
+
+export default Experience;
