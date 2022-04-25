e65851f96a6b621e4c1a87fcca024e6910fd3d20
casibanryan <restyochea@gmail.com>
2022-04-25 13:34:52 +0800

    updating

--- src/include/Services.js
+++ src/include/Services.js
@@ -47,7 +47,7 @@ const Services = () => (
                         <div className="card-customize service-wrapper rounded border-0 shadow p-4">
                             <div className="icon text-center text-primary h1 shadow rounded bg-services">
                                 <span className="header-uim-svg" ><svg xmlns="http://www.w3.org/2000/svg"
-                                    enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="1em">
+                                    enableBackground="new 0 0 24 24" viewBox="0 0 24 24" width="1em">
                                     <path className="uim-quaternary"
                                         d="M15,2c-3.3772,0.00142-6.27155,2.41462-6.88025,5.73651c2.90693-1.59074,6.553-0.52375,8.14374,2.38317c0.98206,1.79462,0.98206,3.96594,0,5.76057c3.8013-0.69634,6.31837-4.3424,5.62202-8.14369C21.27662,4.41261,18.37925,1.99872,15,2z">
                                     </path>
@@ -68,7 +68,7 @@ const Services = () => (
                             </div>
                             <div className="big-icon h1 text-primary">
                                 <span className="uim-svg" ><svg xmlns="http://www.w3.org/2000/svg"
-                                    enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="1em">
+                                    enableBackground="new 0 0 24 24" viewBox="0 0 24 24" width="1em">
                                     <path className="uim-quaternary"
                                         d="M15,2c-3.3772,0.00142-6.27155,2.41462-6.88025,5.73651c2.90693-1.59074,6.553-0.52375,8.14374,2.38317c0.98206,1.79462,0.98206,3.96594,0,5.76057c3.8013-0.69634,6.31837-4.3424,5.62202-8.14369C21.27662,4.41261,18.37925,1.99872,15,2z">
                                     </path>
@@ -247,7 +247,7 @@ const Services = () => (
                 </div>
             </div>
         </section>
-        <hr classNameName="m-0" />
+        <hr className="m-0" />
 
     </>
 )
