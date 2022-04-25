65bfff3674436ebc54573d2ad13106899b07c6e0
casibanryan <restyochea@gmail.com>
2022-04-25 08:31:38 +0800

    updating

--- src/include/Navbar.js
+++ src/include/Navbar.js
@@ -1,11 +1,13 @@
 import '../css/styles.css';
+import profile from '../assets/img/profile.png';
+
 
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <span className="d-block d-lg-none">Resty Ochea</span>
             <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2"
-                src="assets/img/profile.png" alt="profile" /></span>
+                src={profile} alt="profile" /></span>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
             aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
