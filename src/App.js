ecce827da59d70762d8bcc56ae267a257ec34b53
casibanryan <restyochea@gmail.com>
2022-04-25 09:33:34 +0800

    updating

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
           type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-        <script src="../js/scripts.js" type="text/javascript" />
+        <script src="./js/scripts.js" type="text/javascript" />
       </Helmet>
     </div>
   );
