1db4d0aa16a867c1c4aaae66bebf188dff38ba1d
casibanryan <restyochea@gmail.com>
2022-04-26 09:15:29 +0800

    debugging

--- src/App.js
+++ src/App.js
@@ -23,7 +23,31 @@ function App() {
   //   }
   // }, []);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const sideNav = document.body.querySelector('#sideNav');
+  //   if (sideNav) {
+  //     new bootstrap.ScrollSpy(document.body, {
+  //       target: '#sideNav',
+  //       offset: 74,
+  //     });
+  //   };
+
+  //   const navbarToggler = document.body.querySelector('.navbar-toggler');
+  //   const responsiveNavItems = [].slice.call(
+  //     document.querySelectorAll('#navbarResponsive .nav-link')
+  //   );
+  //   responsiveNavItems.map(function (responsiveNavItem) {
+  //     responsiveNavItem.addEventListener('click', () => {
+  //       if (window.getComputedStyle(navbarToggler).display !== 'none') {
+  //         navbarToggler.click();
+  //       }
+  //     });
+  //   });
+
+  // }, [])
+
+  window.addEventListener('DOMContentLoaded', event => {
+
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
       new bootstrap.ScrollSpy(document.body, {
@@ -44,7 +68,8 @@ function App() {
       });
     });
 
-  }, [])
+  });
+
 
 
   return (
