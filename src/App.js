79312fe2544c0ee849f9c527dfe0f261bf41c2f2
casibanryan <restyochea@gmail.com>
2022-04-26 08:59:05 +0800

    fixing the bug

--- src/App.js
+++ src/App.js
@@ -13,15 +13,38 @@ import React, { useEffect } from 'react';
 
 function App() {
 
+  // useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.src = "./js/scripts.js";
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   }
+  // }, []);
+
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "./js/scripts.js";
-    script.async = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, []);
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+      new bootstrap.ScrollSpy(document.body, {
+        target: '#sideNav',
+        offset: 74,
+      });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+          navbarToggler.click();
+        }
+      });
+    });
+
+  }, [])
 
 
   return (
