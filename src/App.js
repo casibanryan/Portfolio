b4080ff054b5677eb3b3a23bb677dcca9e586809
Resty Ochea <67282968+casibanryan@users.noreply.github.com>
2026-04-05 00:54:24 +0800

    added meta tags for seo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import Navbar from './include/Navbar';
-import About from './include/About';
-import Experience from './include/Experience';
-import Education from './include/Education';
-import Skills from './include/Skills';
-import Interest from './include/Interest';
-import Portfolio from './include/Portfolio';
-import Services from './include/Services';
-import { Helmet } from 'react-helmet';
-import React, { useEffect } from 'react';
+/* eslint-disable array-callback-return */
+import Navbar from './include/Navbar'
+import About from './include/About'
+import Experience from './include/Experience'
+import Education from './include/Education'
+import Skills from './include/Skills'
+import Interest from './include/Interest'
+import Portfolio from './include/Portfolio'
+import Services from './include/Services'
+import { Helmet } from 'react-helmet'
+import { useEffect } from 'react'
 
 function App() {
-
   useEffect(() => {
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
-    );
+    const navbarToggler = document.body.querySelector('.navbar-toggler')
+    const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'))
     responsiveNavItems.map(function (responsiveNavItem) {
       responsiveNavItem.addEventListener('click', () => {
         if (window.getComputedStyle(navbarToggler).display !== 'none') {
-          navbarToggler.click();
+          navbarToggler.click()
         }
-      });
-    });
+      })
+    })
   }, [])
 
   return (
@@ -37,14 +35,32 @@ function App() {
       <Portfolio />
 
       <Helmet>
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
-          type="text/css" />
+        <title>Resty Ochea | Full Stack Developer in Cebu, Philippines</title>
+        <meta name="title" content="Resty Ochea | Full Stack Developer in Cebu, Philippines" />
+        <meta
+          name="description"
+          content="Full Stack Developer with 5 years of experience specializing in Laravel, Vue.js, and React.js. Building scalable web and mobile solutions in Cebu, Philippines."
+        />
+        <meta
+          name="keywords"
+          content="Full Stack Developer, Cebu, Philippines, Laravel, PHP, React, Resty Ochea, Web Developer Philippines"
+        />
+        <meta name="author" content="Resty Ochea" />
+        <meta name="robots" content="index, follow" />
+
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@4.6.0/fonts/remixicon.css" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700"
+          rel="stylesheet"
+          type="text/css"
+        />
         <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
       </Helmet>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
