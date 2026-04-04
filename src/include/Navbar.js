79f7df6c3dc1d052ecb33f71e4c2ae8f0aea4026
Resty Ochea <67282968+casibanryan@users.noreply.github.com>
2026-04-05 00:55:14 +0800

    updates

--- src/include/Navbar.js
+++ src/include/Navbar.js
@@ -13,12 +13,14 @@ const Navbar = () => {
     { name: 'Portfolio', href: '#portfolio' }
   ]
 
-  const mapNames = navList.map(nav => nav.name)
+  const mapNames = navList.map(nav => nav.href.replace('#', ''))
+  const name = 'Resty Ochea'
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
-          <span className="d-block d-lg-none">Resty Ochea</span>
+          <span className="d-block d-lg-none">{name}</span>
           <span className="d-none d-lg-block">
             <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="profile" />
           </span>
@@ -36,7 +38,7 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav">
-            <Scrollspy items={mapNames} currentClassName="isCurrent">
+            <Scrollspy items={mapNames} currentClassName="isCurrent" className="ps-0">
               {navList.map((nav, idx) => (
                 <li key={idx} className="nav-item">
                   <a className="nav-link js-scroll-trigger" href={nav.href}>
