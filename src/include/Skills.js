91b81e44d3026cff0c4b4d6f0884ad3bb36435db
Resty Ochea <67282968+casibanryan@users.noreply.github.com>
2026-04-05 00:54:06 +0800

    organize skills

--- src/include/Skills.js
+++ src/include/Skills.js
@@ -1,44 +1,101 @@
+const list = [
+  {
+    title: 'frontend',
+    items: [
+      {
+        class: 'fab fa-react',
+        name: 'React'
+      },
+      {
+        class: 'fab fa-vuejs',
+        name: 'Vue'
+      },
+      {
+        class: 'fab fa-html5',
+        name: 'HTML'
+      },
+      {
+        class: 'fab fa-css3',
+        name: 'CSS'
+      },
+      {
+        class: 'fab fa-sass',
+        name: 'Sass'
+      },
+      {
+        class: 'fab fa-bootstrap',
+        name: 'Bootstrap'
+      },
+      {
+        class: 'ri-tailwind-css-fill',
+        name: 'Tailwind'
+      },
+      {
+        class: 'fab fa-js-square',
+        name: 'Javascript'
+      }
+    ]
+  },
+  {
+    title: 'backend',
+    items: [
+      {
+        class: 'fab fa-laravel',
+        name: 'Laravel'
+      },
+      {
+        class: 'fab fa-node',
+        name: 'Node'
+      },
+      {
+        class: 'fab fa-python',
+        name: 'Python'
+      },
+      {
+        class: 'fab fa-php',
+        name: 'PHP'
+      }
+    ]
+  }
+]
+
+const workFlows = [
+  'Mobile-First & Adaptive Architectures',
+  'Agile Collaboration & Technical Leadership',
+  'Legacy Refactoring & UI/UX Modernization'
+]
 
 const Skills = () => (
-    <>
-        <section className="resume-section" id="skills">
-            <div className="resume-section-content">
-                <h2 className="mb-5">Skills</h2>
-                <div className="subheading mb-3">Programming Languages & Tools</div>
-                <ul className="list-inline dev-icons">
-                    <li className="list-inline-item"><i className="fab fa-html5"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-npm"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-react"></i></li>
-                    <li className="list-inline-item"><i className="fa-brands fa-bootstrap"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-sass"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-wordpress"></i></li>
-                    <li className="list-inline-item"><i className="fa-brands fa-laravel"></i></li>
-                    <li className="list-inline-item"><i className="fa-brands fa-python"></i></li>
-                </ul>
-                <div className="subheading mb-3">Workflow</div>
-                <ul className="fa-ul mb-0">
-                    <li>
-                        <span className="fa-li"><i className="fas fa-check"></i></span>
-                        Mobile-First, Responsive Design
-                    </li>
-                    <li>
-                        <span className="fa-li"><i className="fas fa-check"></i></span>
-                        Cross Browser Testing & Debugging
-                    </li>
-                    <li>
-                        <span className="fa-li"><i className="fas fa-check"></i></span>
-                        Cross Functional Teams
-                    </li>
-                    <li>
-                        <span className="fa-li"><i className="fas fa-check"></i></span>
-                        Redesign
-                    </li>
-                </ul>
-            </div>
-        </section>
-    </>
+  <>
+    <section className="resume-section" id="skills">
+      <div className="resume-section-content">
+        <h2 className="mb-5">Skills</h2>
+        {list.map((skill, i) => (
+          <div key={i}>
+            <div className="subheading mb-3">{skill.title}</div>
+            <ul className="list-inline dev-icons">
+              {skill.items.map((item, index) => (
+                <li className="list-inline-item" key={index}>
+                  <i className={item.class}></i>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <div className="subheading mb-3">Workflow</div>
+        <ul className="fa-ul mb-0">
+          {workFlows.map((flow, i) => (
+            <li key={i}>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {flow}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  </>
 )
 
-export default Skills;
+export default Skills
